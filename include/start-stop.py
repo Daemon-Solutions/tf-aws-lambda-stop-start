a85629405f1d173896ddaf6d17f0a5ec6b588287
James Hollins <james.hollins@bashton.com>
2017-05-03 16:43:57 +0100

    Updated start-stop.py to make readable

--- include/start-stop.py
+++ include/start-stop.py
@@ -12,16 +12,18 @@ def toggle_instances(start):
     ec2_client = boto3.client("ec2", region_name="eu-west-1")
     action_name = 'start' if start else 'stop'
     print('Looking for instances to {}'.format(action_name))
-    # Look for the tag stopAtNight
-    description = ec2_client.describe_instances( Filters=[ { 'Name': 'instance-state-name', 
-        'Values': ['stopped' if start else 'running'] },
-        { 'Name': 'tag:Scheduled-Stop-Start', 'Values' : ['yes'] }])
+    # Look for the tag Scheduled-Stop-Start
+    description = ec2_client.describe_instances(
+    Filters=[
+        { 'Name': 'instance-state-name', 'Values': ['stopped' if start else 'running'] },
+        { 'Name': 'tag:Scheduled-Stop-Start', 'Values' : ['yes']}
+    ]
+)
     
     for reservation in description["Reservations"]:
         for instance in reservation["Instances"]:
             print('instance to {}: {}'.format(action_name, instance["InstanceId"]))
             toggle_instance(instance, start)
-
-        
+       
 def lambda_handler(event, context):
     toggle_instances("wakeup" in event["resources"][0])
